37c146b905d8d9ef38155d3dd08af238ac630b32
idstev <johaanc002@gmail.com>
2023-05-11 17:47:47 -0500

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { OfertasComponent } from './components/ofertas/ofertas.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ContactosComponent } from './components/contactos/contactos.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { E404Component } from './components/e404/e404.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OfertasComponent,
+    NosotrosComponent,
+    ContactosComponent,
+    NavbarComponent,
+    FooterComponent,
+    E404Component,
+    ProductosComponent
   ],
   imports: [
     BrowserModule,
